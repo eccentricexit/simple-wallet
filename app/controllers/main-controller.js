034dbf081d2135dd48918d154a00f05eeeb9437b
Matheus Faria <mtsalenc@gmail.com>
2017-06-10 17:44:09 -0300

    feat: Add Balance view

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,4 +1,8 @@
-function MainController(){
+function MainController($scope){
   var vm = this;
-  vm.someName="";
+  SimpleWallet.deployed().then(function(instance){
+      vm.balance = web3.eth.getBalance(instance.address).toNumber();
+      vm.balanceInEther = web3.fromWei(vm.balance,"ether");
+      $scope.$apply();
+  });
 }
