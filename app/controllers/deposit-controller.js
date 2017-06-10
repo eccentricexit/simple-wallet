30d21b7301458db37b380c6f3edf4456f20a86ba
Matheus Faria <mtsalenc@gmail.com>
2017-06-10 17:44:32 -0300

    feat: add Deposit view

---
+++ app/controllers/deposit-controller.js
@@ -0,0 +1,27 @@
+function SendFundsController($scope) {
+  var vm = this;
+  vm.accounts = web3.eth.accounts;
+
+
+  vm.depositFunds = function(address_from, amount_from) {
+
+    SimpleWallet.deployed().then(function(instance){
+      var contract = instance;
+
+      web3.eth.sendTransaction({
+        from: address_from,
+        to: contract.address,
+        value: web3.toWei(amount_from, "ether")
+      }, function(error, result) {
+        if(error){
+          vm.has_errors = true;
+        }else{
+          vm.transfer_success = true;
+        }
+        $scope.$apply();
+      });
+    });
+
+  }
+
+}
