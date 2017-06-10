390ca8e983a91bd8ca7e1070db447bf44b500a45
Matheus Faria <mtsalenc@gmail.com>
2017-06-10 17:48:08 -0300

    refactor: Moved code from separate files

--- app/app.js
+++ app/app.js
@@ -1,1 +1,28 @@
+var app = angular.module("simpleWallet", ['ngRoute']);
 
+app.controller("SendFundsController", SendFundsController);
+app.controller("EventsController", EventsController);
+app.controller("MainController", MainController);
+app.controller("WithdrawalController", WithdrawalController);
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/main.html',
+      controller: 'MainController'
+    })
+    .when('/deposit',{
+      templateUrl: 'views/deposit.html',
+      controller: 'SendFundsController'
+    })
+    .when('/events',{
+      templateUrl: 'views/events.html',
+      controller: 'EventsController'
+    })
+    .when('/withdrawl',{
+      templateUrl: 'views/withdrawal.html',
+      controller: 'WithdrawalController'
+    })
+    .otherwise({
+      templateUrl: 'views/four-oh-four.html'
+    });
+});
