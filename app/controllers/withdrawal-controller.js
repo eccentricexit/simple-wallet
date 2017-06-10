142418e5c4b61b94c832af0848084d05b08f41ad
Matheus Faria <mtsalenc@gmail.com>
2017-06-10 18:41:18 -0300

    feat: Add withdrawal function

--- app/controllers/withdrawal-controller.js
+++ app/controllers/withdrawal-controller.js
@@ -1,3 +1,24 @@
-function WithdrawalController($scope){
+function WithdrawalController($scope) {
   var vm = this;
+  vm.accounts = web3.eth.accounts;
+
+  vm.withdrawlFunds = function(address, amount) {
+    SimpleWallet.deployed()
+      .then(function(instance) {
+        instance.sendFunds(web3.toWei(amount, "ether"), address, {
+            from: web3.eth.accounts[0],
+            gas: 200000
+          })
+          .then(function(transactionReceipt) {
+            console.log(transactionReceipt.tx);
+            vm.transfer_success = true;
+            $scope.$apply();
+          }).catch(function(error) {
+            console.error(error);
+            vm.has_errors = error;
+            $scope.$apply();
+          });
+      });
+
+  }
 }
