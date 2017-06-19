748f6b1b33086f3e3bec255f843f77032da440e1
Matheus Faria <mtsalenc@gmail.com>
2017-06-19 18:39:43 -0300

    feat: Add events view

--- app/controllers/events-controller.js
+++ app/controllers/events-controller.js
@@ -1,3 +1,21 @@
 function EventsController($scope){
   var vm = this;
+  vm.events = [];
+
+  SimpleWallet.deployed()
+    .then(function(instance) {
+
+      var eventsToWatch = instance.allEvents({fromBlock: 0, toBlock: 'latest'});
+
+      eventsToWatch.watch(function(error,result){
+        vm.events.push(result);
+        $scope.$apply();
+      });
+
+  });
+
+  $scope.$on('destroy',function(){
+    eventsToWatch.stopWatching();
+  });
+
 }
