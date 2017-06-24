68d4498ad89ff11ce72ba51e5cecc0d8d9ded853
Matheus Faria <mtsalenc@gmail.com>
2017-06-24 19:57:50 -0300

    fix: Switched to asynchronous call for better Metamask UX

--- app/controllers/main-controller.js
+++ app/controllers/main-controller.js
@@ -1,8 +1,14 @@
-function MainController($scope){
+function MainController($scope) {
   var vm = this;
-  SimpleWallet.deployed().then(function(instance){
-      vm.balance = web3.eth.getBalance(instance.address).toNumber();
-      vm.balanceInEther = web3.fromWei(vm.balance,"ether");
-      $scope.$apply();
+  SimpleWallet.deployed().then(function(instance) {
+    web3.eth.getBalance(instance.address, function(error, result) {
+      if (!error) {
+        vm.balance = result.toNumber();
+        vm.balanceInEther = web3.fromWei(vm.balance, "ether");
+        $scope.$apply();
+      } else {
+        console.log(error);
+      }
+    })
   });
 }
