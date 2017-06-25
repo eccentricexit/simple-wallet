42645c195ec7581170dcef9f26264b0696170987
Matheus Faria <mtsalenc@gmail.com>
2017-06-25 15:06:31 -0300

    fix: Remove redundant information

--- app/js/window-web3-attacher.js
+++ app/js/window-web3-attacher.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', function() {
   // Checking if Web3 has been injected by the browser (Mist/MetaMask)
   if (typeof web3 !== 'undefined') {
-    console.warn("Using web3 detected from external source. If using MetaMask, see the following link. Feel free to delete this warning. :) http://truffleframework.com/tutorials/truffle-and-metamask")
+    console.warn("Using web3 detected from external source.")
     // Use Mist/MetaMask's provider
     window.web3 = new Web3(web3.currentProvider);
   } else {
