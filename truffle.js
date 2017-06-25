255f9f9037dae9e26543005f8ddfc7bdb8b6e0fa
Matheus Faria <mtsalenc@gmail.com>
2017-06-25 15:05:52 -0300

    docs: Remove obsolete addresses

--- truffle.js
+++ truffle.js
@@ -8,14 +8,7 @@ module.exports = {
     rinkeby: {
       host: "localhost",
       port: 8545,
-      network_id: "*",
+      network_id: "4",
     }
   }
 };
-
-/*
-Migrations address on rinkeby: 0x9cf2d9e7ebc9752db47aa7e507c1bb71e749f9a5
-SimpleWallet address on rinkeby: 0xef53ddb0d863ddcc04b2571b286e78d3f2f26c84
-
-swarm frontend address: /bzz:/a1d425654b55662f76bd0cd558208984ac629fc9d9852f3b2224c74028c9c25e/index.html
-*/
