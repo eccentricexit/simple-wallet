c33a83c74fd407a10b6a9adb1600e1a1e9f64bc8
Matheus Faria <mtsalenc@gmail.com>
2017-06-24 23:07:13 -0300

    feat: Add front-end check for account without permission

--- app/controllers/deposit-controller.js
+++ app/controllers/deposit-controller.js
@@ -1,26 +1,34 @@
 function SendFundsController($scope) {
   var vm = this;
   vm.accounts = web3.eth.accounts;
-  vm.depositFunds = function(address_from, amount_from) {
+  vm.isAllowedToSendFunds = true;
+  vm.selectedAccount = web3.eth.accounts[0];
 
-    SimpleWallet.deployed().then(function(instance){
-      var contract = instance;
+  vm.depositFunds = function(address_from, amount) {
+    var contract;
+    SimpleWallet.deployed().then(function(instance) {
+      contract = instance;
+      vm.selectedAccount = address_from;
+      return instance.isAllowedToSend(address_from);
+    }).then(function(result) {
+      vm.isAllowedToSendFunds = result;
+
+      if (!vm.isAllowedToSendFund) return;
 
       web3.eth.sendTransaction({
         from: address_from,
         to: contract.address,
-        value: web3.toWei(amount_from, "ether")
+        value: web3.toWei(amount, "ether")
       }, function(error, result) {
-        if(error){
+        if (error) {
           vm.has_errors = true;
           console.log(error);
-        }else{
+        } else {
           vm.transfer_success = true;
         }
-        $scope.$apply();
       });
+      
+      $scope.$apply();
     });
-
-  }
-
+  };
 }
--- app/controllers/withdrawal-controller.js
+++ app/controllers/withdrawal-controller.js
@@ -1,24 +1,37 @@
 function WithdrawalController($scope) {
   var vm = this;
   vm.accounts = web3.eth.accounts;
+  vm.isAllowedToWithdrawFunds = true;
+  vm.selectedAccount = vm.accounts[0];
 
   vm.withdrawlFunds = function(address, amount) {
+    vm.selectedAccount = address;
+
     SimpleWallet.deployed()
       .then(function(instance) {
-        instance.sendFunds(web3.toWei(amount, "ether"), address, {
-            from: web3.eth.accounts[0],
-            gas: 200000
-          })
-          .then(function(transactionReceipt) {
-            console.log(transactionReceipt.tx);
-            vm.transfer_success = true;
-            $scope.$apply();
-          }).catch(function(error) {
-            console.error(error);
-            vm.has_errors = error;
-            $scope.$apply();
-          });
-      });
+        vm.instance = instance;
+        return instance.isAllowedToSend(address);
+      })
+      .then(function(result) {
+        vm.isAllowedToWithdrawFunds = result;
 
+        if (result) {
+          vm.instance.sendFunds(web3.toWei(amount, "ether"), address, {
+              from: web3.eth.accounts[0],
+              gas: 200000
+            })
+            .then(function(transactionReceipt) {
+              console.log(transactionReceipt.tx);
+              vm.transfer_success = true;
+              $scope.$apply();
+            }).catch(function(error) {
+              console.error(error);
+              vm.has_errors = error;
+              $scope.$apply();
+            });
+        }else{
+          $scope.$apply();
+        }
+      });
   }
 }
